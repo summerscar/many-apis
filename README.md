de5f546c7438ff5ccd66272f6cb013e53b926694
shenyu <shen.yus@163.com>
2018-11-29 14:39:21 +0800

    update ip readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # many-apis
-# many-apis
+
+* GET http://ip:port/bangumi/无法成为野兽的我们
+* GET http://ip:port/bingPicture
+* GET http://ip:port/getIPLocal/8.8.8.8
+* GET http://ip:port/
--- controllers/getIPlocal.js
+++ controllers/getIPlocal.js
@@ -1,8 +1,8 @@
 const request = require('request-promise')
 
 let getIPLocal = async (ctx, next) => {
-    let ret = await request(`http://ip.taobao.com/service/getIpInfo.php?ip=${ctx.query.ip}`)
+    let ret = await request(`http://ip.taobao.com/service/getIpInfo.php?ip=${ctx.params.ip}`)
     ctx.body = ret
   }
 
-module.exports = {'GET /getIPLocal': getIPLocal }
+module.exports = {'GET /getIPLocal/:ip': getIPLocal }
